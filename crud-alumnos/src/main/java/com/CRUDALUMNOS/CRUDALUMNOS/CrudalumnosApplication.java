5c38e6f5c9721644ed4103c77612b5e7d12466f2
Ivan Sosa <ivandev2675@gmail.com>
2025-08-15 20:54:19 -0500

    Primer good comit

---
+++ crud-alumnos/src/main/java/com/CRUDALUMNOS/CRUDALUMNOS/CrudalumnosApplication.java
@@ -0,0 +1,13 @@
+package com.CRUDALUMNOS.CRUDALUMNOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudalumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudalumnosApplication.class, args);
+	}
+
+}
---
+++ crud-alumnos/src/test/java/com/CRUDALUMNOS/CRUDALUMNOS/CrudalumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRUDALUMNOS.CRUDALUMNOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudalumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
